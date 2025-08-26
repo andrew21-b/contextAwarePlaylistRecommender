563eb5b3b7bf264fb78ee715d3ef39378057c5ec
Andrew Boateng <andrew.21@outlook.com>
2025-08-26 03:01:23 +0100

    Updates context llm to return a local playlist name when the openai client is not working

--- api/services/context_llm_service.py
+++ api/services/context_llm_service.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from openai import OpenAI
 from api.context.config import settings
+from api.services.context_rules_service import pick_playlist
 
 client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
@@ -10,6 +11,7 @@ def infer_mood(
     calendar_event: Optional[str],
     location: Optional[str],
     social_post: Optional[str],
+    playlists: Optional[list],
 ) -> str:
     prompt = f"""
     You are a music mood assistant. Based on context, return ONE mood keyword.
@@ -32,10 +34,18 @@ def infer_mood(
         response = client.chat.completions.create(
             model="gpt-4o-mini", messages=[{"role": "user", "content": prompt}]
         )
+        mood = response.choices[0].message.content
+        if mood != None:
+            return mood.strip().lower()
     except Exception as e:
         return f"Mood is empty, Error occurred: {e}"
-
-    mood = response.choices[0].message.content
-    if mood != None:
-        return mood.strip().lower()
-    return "Mood is empty"
+    finally:
+        return pick_playlist(
+            {
+                "event": calendar_event,
+                "location": location,
+                "time_of_day": time_of_day,
+                "social_post": social_post,
+            },
+            playlists,
+        )
