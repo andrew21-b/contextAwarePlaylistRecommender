e4d5de47d28383bcaf1e506a0e73cd6f551a36bf
Andrew Boateng <andrew.21@outlook.com>
2025-08-26 03:01:57 +0100

    Updated recommend endpoint to use generative playllst functionality

--- api/controller/v1/endpoints/recommender.py
+++ api/controller/v1/endpoints/recommender.py
@@ -1,15 +1,30 @@
-import json
-from fastapi import APIRouter
-from api.services.recommender_service import recommend_for_context
+from typing import Optional
+from fastapi import APIRouter, Query
+from api.services.playlist_generator_service import generate_mood_playlist
 
 
 router = APIRouter()
 
-with open("data/spotify_playlists.json") as f:
-    playlists = json.load(f)["playlists"]
-
 
 @router.get("/")
-def get_recommendations(location: str, time_of_day: str, event: str):
-    context = {"location": location, "time_of_day": time_of_day, "event": event}
-    return recommend_for_context(context, playlists)
+def get_recommendations(
+    time_of_day: Optional[str] = Query(
+        None, description="Time of day (e.g. 06:00, 15:00:00, 18:00, 22:00:00)"
+    ),
+    calendar_event: Optional[str] = Query(
+        None, description="Current or next calendar event (e.g. Meeting with Bob)"
+    ),
+    location: Optional[str] = Query(
+        None, description="Current location (e.g., Home, Office, Park)"
+    ),
+    social_post: Optional[str] = Query(None, description="Recent social media post"),
+):
+
+    playlist = generate_mood_playlist(
+        time_of_day=time_of_day,
+        calendar_event=calendar_event,
+        location=location,
+        social_post=social_post,
+    )
+
+    return playlist
